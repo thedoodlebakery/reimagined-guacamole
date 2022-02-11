463b5fc680b9a6890df8b75dc701f789192c266c
Paula C <thedoodlebakery@gmail.com>
2022-02-11 12:08:17 -0600

    Adding divisors exercise. Problem and solution.

---
+++ divisors/divisors.py
@@ -0,0 +1,12 @@
+# Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+
+userNumber = input("Want to know what the divisors are for a certain number? Type it here. \n")
+x = range(1, 101);
+numberDivisors = []
+
+
+for num in x:
+    if int(userNumber)%num == 0:
+        numberDivisors.append(num)
+
+print (numberDivisors);
