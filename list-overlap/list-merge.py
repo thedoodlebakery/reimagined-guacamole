943e647fdc8ebc7ff36b666028543b273d958cd6
Paula C <thedoodlebakery@gmail.com>
2022-02-13 23:04:09 -0600

    Adding a exercise solution that combines common numbers from two arrays into one.

---
+++ list-overlap/list-merge.py
@@ -0,0 +1,33 @@
+# Take two lists, say for example these two:
+
+#   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#   b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+
+# Extras:
+
+# Randomly generate two lists to test this
+# Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+result = []
+        
+for num in a:
+    if num in a and num in b:
+        result.append(num)
+
+result = list(set(result))
+print (result)
+       
+
+
+listOne = range(1, 90)
+listTwo = range(1, 51)
+newList = []
+
+for n in listOne:
+    if n in listOne and n in listTwo:
+        newList.append(n);
+        
+print (newList);
