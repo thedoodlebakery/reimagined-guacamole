a921a002fab984d0ce7b30c0e079b7c158ee46f5
Paula C <thedoodlebakery@gmail.com>
2022-02-17 12:23:51 -0600

    Added a function for player one if they want to play with the computer. They can also move past that and play with another person.

--- game-rps/rps.py
+++ game-rps/rps.py
@@ -5,20 +5,33 @@
 # Rock beats scissors
 # Scissors beats paper
 # Paper beats rock
-moveSet = ["rock", "paper", "scissors"]
+import random
 
 # Player Inputs
-playerOne = input("Do you choose rock, paper or scissors? \n")
-playerTwo = input("Do you choose rock, paper or scissors? \n")
+playerOne = input("Do you choose rock, paper or scissors? \n");
+playComputer = input("Do you want to play with the computer? \n");
+
 computerPlayer = ""
 
-# while playerTwo != "":
-#     playerTwo
-#     break
 
+def computerChoice():
+    computerAnswer = ["rock", "paper", "scissors"];
+    global computerPlayer;
+    computerPlayer = random.choice(computerAnswer);
+    print ("Player two answers: \n" + computerPlayer);
 
+if playComputer == 'yes':
+    computerChoice();
+    global playerTwo;
+    playerTwo = computerPlayer
+    #print (playComputer)
+else:
+    playerTwo = input("Do you choose rock, paper or scissors? \n");
+    
+    
 
 def game():
+    # playerTwo = computerPlayer;
     if (playerOne == "rock" and playerTwo == "paper") | (playerOne == "scissors" and playerTwo == "rock") | (playerOne == "paper" and playerTwo == "scissors"):
         print("Player Two wins!");
     elif (playerOne == "paper" and playerTwo == "rock") | (playerOne == "rock" and playerTwo == "scissors") | (playerOne == "scissors" and playerTwo == "paper"):
@@ -26,7 +39,7 @@ def game():
     elif playerOne == playerTwo:
         print("It's a draw!");
     else:
-        print("Please check your answer and try again.")
+        print("Please check your answer and try again.");
 
 game();
    
